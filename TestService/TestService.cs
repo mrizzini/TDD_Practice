6d162f3dc5b95b3e4de36efeb0afb1fabd1b0b5e
Matthew Rizzini <matthewrizzini@gmail.com>
2018-08-05 16:30:08 -0400

    adding in string calculator tests

--- TestService/TestService.cs
+++ TestService/TestService.cs
@@ -28,6 +28,44 @@ namespace Test.Services
             return fizzer;
         }
 
+       public int StringCalculator(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return 0;
+            
+            if (str.Contains("-"))
+                throw new System.ArgumentException("Negative number");
+
+            if(str.Contains(",") || str.Contains("\r\n"))
+            {
+                var sum =0;
+                char[] delimiters = new char[] { '\r', '\n', ',' };
+                String [] intString = str.Split(delimiters,StringSplitOptions.RemoveEmptyEntries);
+                foreach(var n in intString)
+                {
+                    var inputNum = Int32.Parse(n);
+                    if(inputNum<1000)
+                        sum+=inputNum;
+                }
+                return sum;
+            }
+
+            // if(str.Contains(","))
+            // {
+            //     String[] substrings = str.Split(',');
+            //     return Int32.Parse(substrings[0]) + Int32.Parse(substrings[1]);
+            // }
+
+            // if (str.Contains("\r\n"))
+            // {
+            //     String[] newLineSubStrings = str.Split('\n');
+            //     return Int32.Parse(newLineSubStrings[0]) + Int32.Parse(newLineSubStrings[1]);
+            // }
+
+            return Int32.Parse(str);
+
+        }
+
 
     }
 }
